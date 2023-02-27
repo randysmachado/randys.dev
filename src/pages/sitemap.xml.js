0926c5dfd4eab8467a2782c9c941599daaddc22a
Randys Machado <randys.machado@gmail.com>
2023-02-27 09:53:13 -0300

    chore: add pages on sitemap.xml

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,16 +1,24 @@
 import { getAllPosts } from 'lib/posts'
+import { getPagePaths } from 'lib/readingPages'
 
 const BLOG_URL = 'https://randys.dev'
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${`${BLOG_URL}`}</loc>
+        <priority>1.0</priority>
+        <changefreq>daily</changefreq>
+        <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
     ${posts
       .map(({ slug }) => {
         return `
       <url>
           <loc>${`${BLOG_URL}/${slug}`}</loc>
-          <priority>0.3</priority>
+          <priority>1.0</priority>
+          <changefreq>daily</changefreq>
           <lastmod>${new Date().toISOString()}</lastmod>
       </url>
     `
@@ -24,7 +32,10 @@ const SiteMap = () => {}
 
 export async function getServerSideProps({ res }) {
   const posts = getAllPosts(['slug'])
-  const sitemap = generateSiteMap(posts)
+  const pages = getPagePaths(['slug'])
+  const all = [...pages, ...posts]
+
+  const sitemap = generateSiteMap(all)
 
   res.setHeader('Content-Type', 'text/xml')
   res.write(sitemap)
