090ecd477100d9884772f0952d3eeef87ec7ace8
Randys Machado <randys.machado@gmail.com>
2024-04-17 14:35:07 -0300

    add tags card

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,6 @@
 import { MetadataRoute } from 'next'
 
 import { posts } from '#site/content'
-import { getAllTags, sortTagsByCount } from '@/lib/utils'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const sitemapPost: MetadataRoute.Sitemap = posts.map((post) => {
@@ -12,22 +11,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: post.date
     }
   })
-
-  // const tags = getAllTags(posts)
-  // const sortedTags = sortTagsByCount(tags)
-
-  // const sitemapTags: MetadataRoute.Sitemap = posts.map((post) => {
-  //   const tag = sortedTags.map((tag) => {
-  //     return {
-  //       url: `${process.env.NEXT_PUBLIC_APP_URL}/blog/${tag}`,
-  //       priority: 1.0,
-  //       changeFrequency: 'daily',
-  //       lastModified: post.date
-  //     }
-  //   })
-
-  //   return tag
-  // })
 
   return [
     {
@@ -42,7 +25,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'daily',
       lastModified: new Date()
     },
-    ...sitemapPost,
-    ...sitemapTags
+    ...sitemapPost
   ]
 }
